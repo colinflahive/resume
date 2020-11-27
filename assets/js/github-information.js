11ab9b55ecdfec1a9a7618d0c31776f2c46f77c8
colinflahive <cflahive@hotmail.com>
2020-11-27 21:27:20 +0000

    Getting the Repo Data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -27,10 +27,13 @@ $("#gh-user-data").html(
         </div>`);
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondReponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-user-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
