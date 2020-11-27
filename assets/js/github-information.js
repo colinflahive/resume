179f5693d27fb2cec452c1f25fd085541b635f7e
colinflahive <cflahive@hotmail.com>
2020-11-27 21:21:06 +0000

    gitHub with Avatar html

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,19 @@
+function userInformationHTML(user) {
+    return `
+    <h2>${user.name}
+        <span class="small-name">
+            (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+        </span>
+    </h2>
+    <div class="gh-content">
+        <div class="gh-avatar">
+            <a href="${user.html_url} target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" /></a>
+        </div>
+        <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+    </div>` 
+}
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -22,7 +38,7 @@ $("#gh-user-data").html(
         } else {
             console.log(errorResponse);
             $("#gh-user-data").html(
-                `<h2>Error: ${errorReponse.responseJSON.message</h2>`);
+                `<h2>Error: ${errorReponse.responseJSON.message}</h2>`);
         }
     });
 }       
